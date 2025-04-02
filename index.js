68238e1bfb8ead270ebbf250cbd0dcd28539b080
pratham_1248 <mandaveprathamesh5@gmail.com>
2025-04-02 07:06:09 +0000

    finn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 // Example route testing
 app.get('/', (req, res) => {
-    res.send('Hello, world! Your Node.js app is running here.');
+    res.send('Hello, world! Your Node.js app is running here on port 3000.');
 });
 
 app.listen(PORT, () => {
